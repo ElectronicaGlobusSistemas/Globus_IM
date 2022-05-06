09ed325bbe67d2d7a07c73916ac8b50dfc0abc76
fabicorrales91 <fabicorrales91@gmail.com>
2022-05-05 19:04:00 -0500

    Prueba Branch

--- src/main.cpp
+++ src/main.cpp
@@ -8,10 +8,15 @@
 
 void setup() {
   Init_Config();
+  pinMode(2, OUTPUT);
 }
 
 void loop() {
   Rum_Bootloader();
+  digitalWrite(2, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(500);                       // wait for a second
+  digitalWrite(2, LOW);    // turn the LED off by making the voltage LOW
+  delay(500); 
 }
 
 
