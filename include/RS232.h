3700eab83048e1a57eb6b2b99309ea8275d2dbb5
fabicorrales91 <fabicorrales91@gmail.com>
2022-05-18 14:34:50 -0500

    Configuracion Binaria EVEN-ODD

--- include/RS232.h
+++ include/RS232.h
@@ -1,5 +1,4 @@
 #include "Arduino.h"
-
 #include "driver/uart.h"
 
 #define NUMERO_PORTA_SERIALE UART_NUM_2
@@ -7,111 +6,94 @@
 #define RD_BUF_SIZE (1024)
 static QueueHandle_t uart2_queue;
 
-#define     SYNC                  0x080
-#define     POLL                  0x081
-#define     DIR                   0x001
+#define SYNC 0x80
+#define POLL 0x81
+#define DIR 0x01
 
-static const char * TAG = "";
+static const char *TAG = "";
 TaskHandle_t Task1;
 TaskHandle_t Task2;
 #define U2RXD 17
 #define U2TXD 16
 
-uint8_t rxbuf[256];     //Buffer di ricezione
-uint16_t rx_fifo_len;        //Lunghezza dati
+uint8_t rxbuf[256];   // Buffer di ricezione
+uint16_t rx_fifo_len; // Lunghezza dati
 
-
-unsigned  long tiempo_actual = 0;
+unsigned long tiempo_actual = 0;
 unsigned long tiempo_previo = 0;
-unsigned  long tiempo_actual2 = 0;
+unsigned long tiempo_actual2 = 0;
 unsigned long tiempo_previo2 = 0;
-int bandera = 0;
 char dat[1] = {SYNC};
-char dat2[1] = {0x081};
+int conta_bytes = 0;
+
+using namespace std;
 
 static void UART_ISR_ROUTINE(void *pvParameters);
 
-void Init_UART2(){
-
+void Init_UART2()
+{
   Serial.begin(19200);
-  pinMode(2, OUTPUT);
-  pinMode(4,INPUT);
 
-  //Configuro la porta Serial2 (tutti i parametri hanno anche un get per effettuare controlli)
+  // Configuro la porta Serial2 (tutti i parametri hanno anche un get per effettuare controlli)
   uart_config_t Configurazione_UART2 = {
-    .baud_rate = 19200,
-    .data_bits = UART_DATA_8_BITS,
-    .parity = UART_PARITY_DISABLE,
-    .stop_bits = UART_STOP_BITS_1,
-    .flow_ctrl = UART_HW_FLOWCTRL_DISABLE
-  };
+      .baud_rate = 19200,
+      .data_bits = UART_DATA_8_BITS,
+      .parity = UART_PARITY_DISABLE,
+      .stop_bits = UART_STOP_BITS_1,
+      .flow_ctrl = UART_HW_FLOWCTRL_DISABLE};
   uart_param_config(NUMERO_PORTA_SERIALE, &Configurazione_UART2);
 
-
-
-  //Firma: void esp_log_level_set(const char *tag, esp_log_level_tlevel)
+  // Firma: void esp_log_level_set(const char *tag, esp_log_level_tlevel)
   esp_log_level_set(TAG, ESP_LOG_INFO);
 
-
-
-  //Firma: esp_err_tuart_set_pin(uart_port_tuart_num, int tx_io_num, int rx_io_num, int rts_io_num, int cts_io_num)
+  // Firma: esp_err_tuart_set_pin(uart_port_tuart_num, int tx_io_num, int rx_io_num, int rts_io_num, int cts_io_num)
   uart_set_pin(NUMERO_PORTA_SERIALE, U2TXD, U2RXD, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
 
-
-  //Firma: uart_driver_install(UART_NUM_2, uart_buffer_size, uart_buffer_size, 10, &uart_queue, 0));
-  //       uart_driver_install(Numero_porta, RXD_BUFFER, TXD_Buffer, event queue handle and size, flags to allocate an interrupt)
+  // Firma: uart_driver_install(UART_NUM_2, uart_buffer_size, uart_buffer_size, 10, &uart_queue, 0));
+  //        uart_driver_install(Numero_porta, RXD_BUFFER, TXD_Buffer, event queue handle and size, flags to allocate an interrupt)
   uart_driver_install(NUMERO_PORTA_SERIALE, BUF_SIZE, BUF_SIZE, 20, &uart2_queue, 0);
 
-
-  //Create a task to handler UART event from ISR
-  xTaskCreatePinnedToCore(UART_ISR_ROUTINE, "UART_ISR_ROUTINE", 10000, NULL, 12, NULL,1);
-  
+  // Create a task to handler UART event from ISR
+  xTaskCreatePinnedToCore(UART_ISR_ROUTINE, "UART_ISR_ROUTINE", 10000, NULL, 1, NULL, 1);
 }
 
-
-
-
-
-int sendDataa(const char* datos, unsigned int tamano)
+int sendDataa(const char *datos, unsigned int tamano)
 {
-
-
-  // ESP_ERROR_CHECK(uart_wait_tx_done(UART_NUM_2, 1));
+  ESP_ERROR_CHECK(uart_wait_tx_done(UART_NUM_2, 10));
   const int txBytes = uart_write_bytes(UART_NUM_2, datos, tamano);
-  // ESP_ERROR_CHECK(uart_wait_tx_done(UART_NUM_2, 11));
+  ESP_ERROR_CHECK(uart_wait_tx_done(UART_NUM_2, 10));
   return txBytes;
 }
 
-
-void Transmite_Poll( unsigned char Com_SAS )
+void Transmite_Poll(unsigned char Com_SAS)
 {
-  char dat3[1] = {POLL};
-  char dat4[1] = {DIR};
-  char dat5[1] = {Com_SAS};
+  const char dat3[1] = {POLL};
+  const char dat4[1] = {DIR};
+  const char dat5[1] = {Com_SAS};
 
   /**********************************************************************************/
-  //Transmite direccion con bit 9 en 1
-  if ( Com_SAS != 0 )
+  // Transmite direccion con bit 9 en 1
+  if (Com_SAS != 0)
   {
-    sendDataa( dat4, sizeof(dat4) ); //Transmito Byte con 9bit = 1
+    sendDataa(dat4, sizeof(dat4)); // Transmito Byte con 9bit = 1
   }
   else
   {
-    sendDataa( dat3, sizeof(dat3)); //Transmito Byte con 9bit = 1
+    sendDataa(dat3, sizeof(dat3)); // Transmito Byte con 9bit = 1
   }
   /***********************************************************************************/
-  //Retardo de 3ms para inter_byte_delay
+  // Retardo de 3ms para inter_byte_delay
   delay(3);
   /***********************************************************************************/
-  //Transmito Byte de comando
-  if ( Com_SAS != 0 )
+  // Transmito Byte de comando
+  if (Com_SAS != 0)
   {
     sendDataa(dat5, sizeof(dat5));
-      delay( 100 );  //100
+    delay(100); // 100
   }
   else
   {
-     delay( 100 ); // 100
+    delay(100); // 100
   }
 }
 
@@ -121,71 +103,86 @@ static void UART_ISR_ROUTINE(void *pvParameters)
   size_t buffered_size;
   bool exit_condition = false;
 
-  //Infinite loop to run main bulk of task
-  while (1) {
-
-    
-
-    //Loop will continually block (i.e. wait) on event messages from the event queue
-    if (xQueueReceive(uart2_queue, (void * )&event, (portTickType)portMAX_DELAY)) {
-
-      //Handle received event
-      if (event.type == UART_DATA) {
-        
-
-        //  uart_get_wakeup_threshold()
+  // Infinite loop to run main bulk of task
+  while (1)
+  {
+    // Loop will continually block (i.e. wait) on event messages from the event queue
+    if (xQueueReceive(uart2_queue, (void *)&event, 10))
+    {
+      // Handle received event
+      if (event.type == UART_DATA)
+      {
         uint8_t UART2_data[255];
+        uint8_t buffer[32];
         int UART2_data_length = 0;
-        ESP_ERROR_CHECK(uart_get_buffered_data_len(UART_NUM_2, (size_t*)&UART2_data_length));
+        ESP_ERROR_CHECK(uart_get_buffered_data_len(UART_NUM_2, (size_t *)&UART2_data_length));
         UART2_data_length = uart_read_bytes(UART_NUM_2, UART2_data, UART2_data_length, 10);
 
-        // Serial.println("LEN= ");Serial.println(UART2_data_length);
-
-        //  Serial.print("DATA= ");
-        for (byte i = 0; i < UART2_data_length; i++) {
-
-
-          if(UART2_data[i]!=0x00 && UART2_data[i]!=0x1F){
-            Serial.println(UART2_data[i], HEX);
+        for (byte i = 0; i < UART2_data_length; i++)
+        {
+//          if (UART2_data[i] != 0x1F)
+          {
+            //            Serial.print(UART2_data[i], HEX);
+            buffer[i] = UART2_data[i];
+            conta_bytes++;
           }
-          
         }
-       
-        /*
-          if (UART2_data[0]==0x1F){
-          digitalWrite(2,HIGH);
-          }
-          else if(UART2_data[0]==0x2F){
-          digitalWrite(2,LOW);
-          }
-        */
+        Serial.print(" ");
+        for (int i = 0; i < conta_bytes; i++)
+        {
+          Serial.print(buffer[i], HEX);
+        }
+        Serial.print(" ");
+        Serial.println(conta_bytes);
+        conta_bytes = 0;
       }
 
-      //Handle frame error event
-      else if (event.type == UART_FRAME_ERR) {
-        //TODO...
-      }
+      // Handle frame error event
+      // switch (event.type)
+      // {
+      // case UART_BREAK: /*!< UART break event*/
+      //   Serial.print(" UART break event");
+      //   break;
+      // case UART_BUFFER_FULL: /*!< UART RX buffer full event*/
+      //   Serial.print(" UART RX buffer full event");
+      //   break;
+      // case UART_FIFO_OVF: /*!< UART FIFO overflow event*/
+      //   Serial.print(" UART FIFO overflow event");
+      //   break;
+      // case UART_FRAME_ERR: /*!< UART RX frame error event*/
+      //   Serial.print(" UART RX frame error event");
+      //   break;
+      // case UART_PARITY_ERR: /*!< UART RX parity event*/
+      //   Serial.print(" UART RX parity event");
+      //   break;
+      // case UART_DATA_BREAK: /*!< UART TX data and break event*/
+      //   Serial.print(" UART TX data and break event");
+      //   break;
+      // case UART_PATTERN_DET: /*!< UART pattern detected */
+      //   Serial.print(" UART pattern detected");
+      //   break;
+      // case UART_EVENT_MAX:
+      //   Serial.print(" UART event MAX");
+      //   break;
+      // }
 
-      //Keep adding else if statements for each UART event you want to support
-      //else if (event.type == OTHER EVENT) {
-      //TODO...
-      //}
+      // Keep adding else if statements for each UART event you want to support
+      // else if (event.type == OTHER EVENT) {
+      // TODO...
+      // }
 
-
-      //Final else statement to act as a default case
-      else {
-        //TODO...
-      }
+      // Final else statement to act as a default case
     }
 
-    //If you want to break out of the loop due to certain conditions, set exit condition to true
-    if (exit_condition) {
+    // If you want to break out of the loop due to certain conditions, set exit condition to true
+    if (exit_condition)
+    {
       break;
     }
   }
   uart_flush(UART_NUM_2);
-  // uart_set_wakeup_threshold(UART_NUM_2,  1);
+  //   uart_set_wakeup_threshold(UART_NUM_2,  10);
 
-  //Out side of loop now. Task needs to clean up and self terminate before returning
+  // Out side of loop now. Task needs to clean up and self terminate before returning
   vTaskDelete(NULL);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -2,41 +2,91 @@
 //#include "Bootloader.h"
 //#include "Internet.h"
 #include "RS232.h"
-//#include <stdio.h>
+#include <stdio.h>
 //#include <string.h>
 #include "Config_Perifericos.h"
+#include <iostream>
+#include <bitset>
 
+using namespace std;
+using std::bitset;
 
-void setup() {
+int bandera, conta_poll = 0;
+
+void setup()
+{
   Init_Config();
+  pinMode(2, OUTPUT);
 }
 
+void loop()
+{
+  tiempo_actual = millis();
+  tiempo_actual2 = millis();
 
+  if (tiempo_actual - tiempo_previo >= 200)
+  {
+    conta_poll++;
+    tiempo_previo = tiempo_actual;
+    if (bandera == 0 && conta_poll < 50)
+    {
+      const char dato_sync[1] = {128};
+      sendDataa(dato_sync, sizeof(dato_sync)); // transmite sincronización
+      bandera = 1;
+    }
+    else if (bandera == 1 && conta_poll < 50)
+    {
+      const char dato_poll_1[1] = {1};
+      const char dato_poll_2[1] = {129};
 
+      sendDataa(dato_poll_1, sizeof(dato_poll_1));
+      delay(3);
+      sendDataa(dato_poll_2, sizeof(dato_poll_2));
+//      Transmite_Poll(0x00); // transmite general poll
+      bandera = 0;
+    }
+    else if (conta_poll >= 50)
+    {
+      conta_poll = 0;
+      digitalWrite(2, HIGH);
 
-void loop() {
-  
-    tiempo_actual=millis();
-    tiempo_actual2=millis();
+      // TX>= 01 2F 03 00 00 2F 7E F2
 
-
-  
-
-      if(tiempo_actual-tiempo_previo>=100){
-        if(bandera==1){
-        tiempo_previo=tiempo_actual;
-        sendDataa(dat,sizeof(dat)); // transmite sincronización
-        //Transmite_Poll(0x00); // general poll
-        bandera=0;
-        }
-        else{
-            
-            Transmite_Poll(0x00);
-            bandera=1;
-        }
-      }
+      const char dato_1[1] = {0x01};
+      const char dato_2[1] = {0x15};
+      const char dato_3[1] = {0x00};
+      const char dato_4[1] = {0x00};
+      const char dato_5[1] = {0xFF};
+      const char dato_6[1] = {0xE0};
+      const char dato_7[1] = {0x7E};
+      const char dato_8[1] = {0xF2};
+      //      int len = sizeof(dato_1);
+      // for (int i = 0; i < len; i++)
+      // {
+      //   Serial.print(dato_1[i], HEX);
+      // }
+      Serial.print(" - ");
+      
+//      delay(100);
+      sendDataa(dato_1, sizeof(dato_1));
+      delay(3);
+      uart_set_parity(UART_NUM_2, UART_PARITY_ODD);
+      sendDataa(dato_2, sizeof(dato_2));
+      delay(3);
+      // sendDataa(dato_3, sizeof(dato_3));
+      // delay(3);
+      // sendDataa(dato_4, sizeof(dato_4));
+      // delay(3);
+      // sendDataa(dato_5, sizeof(dato_5));
+      // delay(3);
+      // sendDataa(dato_6, sizeof(dato_6));
+      // delay(3);
+      // sendDataa(dato_7, sizeof(dato_7));
+      // delay(3);
+      // sendDataa(dato_8, sizeof(dato_8));
+      delay(100);
+      digitalWrite(2, LOW);
+      uart_set_parity(UART_NUM_2, UART_PARITY_DISABLE);
+    }
+  }
 }
-
-
-
-
