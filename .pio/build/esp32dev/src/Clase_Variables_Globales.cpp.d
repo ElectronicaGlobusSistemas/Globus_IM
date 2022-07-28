d46aa705e9d88b6db5011a1fcc2204ecb0306b84
fabicorrales91 <fabicorrales91@gmail.com>
2022-07-28 14:41:14 -0500

    fixed gitignore

---
+++ .pio/build/esp32dev/src/Clase_Variables_Globales.cpp.d
@@ -0,0 +1,7 @@
+.pio/build/esp32dev/src/Clase_Variables_Globales.cpp.o: \
+ src/Clase_Variables_Globales.cpp include/Clase_Variables_Globales.h \
+ C:/.platformio/packages/framework-arduinoespressif32/cores/esp32/Stream.h \
+ C:/.platformio/packages/framework-arduinoespressif32/cores/esp32/Print.h \
+ C:/.platformio/packages/framework-arduinoespressif32/cores/esp32/WString.h \
+ C:/.platformio/packages/framework-arduinoespressif32/cores/esp32/pgmspace.h \
+ C:/.platformio/packages/framework-arduinoespressif32/cores/esp32/Printable.h
