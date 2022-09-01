f098bc0a07475b364722b31eee47248f6eafa6d2
fabicorrales91 <fabicorrales91@gmail.com>
2022-09-01 15:44:21 -0500

    Fixed 'Fallo'

---
+++ .pio/build/esp32dev/src/Clase_Variables_Globales.cpp.d
@@ -0,0 +1,7 @@
+.pio/build/esp32dev/src/Clase_Variables_Globales.cpp.o: \
+ src/Clase_Variables_Globales.cpp include/Clase_Variables_Globales.h \
+ C:/Users/Program.\ Electronico/.platformio/packages/framework-arduinoespressif32/cores/esp32/Stream.h \
+ C:/Users/Program.\ Electronico/.platformio/packages/framework-arduinoespressif32/cores/esp32/Print.h \
+ C:/Users/Program.\ Electronico/.platformio/packages/framework-arduinoespressif32/cores/esp32/WString.h \
+ C:/Users/Program.\ Electronico/.platformio/packages/framework-arduinoespressif32/cores/esp32/pgmspace.h \
+ C:/Users/Program.\ Electronico/.platformio/packages/framework-arduinoespressif32/cores/esp32/Printable.h
