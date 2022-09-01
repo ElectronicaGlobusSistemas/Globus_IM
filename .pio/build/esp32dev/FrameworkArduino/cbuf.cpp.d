f104d8af3bb57976695aae730ff625d8023b3d06
fabicorrales91 <fabicorrales91@gmail.com>
2022-09-01 16:15:49 -0500

    add .pio to gitignore

--- .pio/build/esp32dev/FrameworkArduino/cbuf.cpp.d
+++ .pio/build/esp32dev/FrameworkArduino/cbuf.cpp.d
@@ -1,3 +1,3 @@
 .pio/build/esp32dev/FrameworkArduino/cbuf.cpp.o: \
- C:/Users/Program.\ Electronico/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
- C:/Users/Program.\ Electronico/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
+ C:/Users/fabic/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
+ C:/Users/fabic/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
--- .pio/build/esp32dev/FrameworkArduino/firmware_msc_fat.c.d
+++ .pio/build/esp32dev/FrameworkArduino/firmware_msc_fat.c.d
@@ -1,3 +1,3 @@
 .pio/build/esp32dev/FrameworkArduino/firmware_msc_fat.c.o: \
- C:/Users/Program.\ Electronico/.platformio/packages/framework-arduinoespressif32/cores/esp32/firmware_msc_fat.c \
- C:/Users/Program.\ Electronico/.platformio/packages/framework-arduinoespressif32/cores/esp32/firmware_msc_fat.h
+ C:/Users/fabic/.platformio/packages/framework-arduinoespressif32/cores/esp32/firmware_msc_fat.c \
+ C:/Users/fabic/.platformio/packages/framework-arduinoespressif32/cores/esp32/firmware_msc_fat.h
