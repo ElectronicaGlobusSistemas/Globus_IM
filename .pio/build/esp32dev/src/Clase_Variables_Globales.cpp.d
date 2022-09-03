6ad0ed1a00f8f3af1ef0d0c99b22064896cf2c7d
fabicorrales91 <fabicorrales91@gmail.com>
2022-09-03 11:14:54 -0500

    Remove .pio

--- .pio/build/esp32dev/src/Clase_Variables_Globales.cpp.d
+++
@@ -1,7 +0,0 @@
-.pio/build/esp32dev/src/Clase_Variables_Globales.cpp.o: \
- src/Clase_Variables_Globales.cpp include/Clase_Variables_Globales.h \
- C:/Users/fabic/.platformio/packages/framework-arduinoespressif32/cores/esp32/Stream.h \
- C:/Users/fabic/.platformio/packages/framework-arduinoespressif32/cores/esp32/Print.h \
- C:/Users/fabic/.platformio/packages/framework-arduinoespressif32/cores/esp32/WString.h \
- C:/Users/fabic/.platformio/packages/framework-arduinoespressif32/cores/esp32/pgmspace.h \
- C:/Users/fabic/.platformio/packages/framework-arduinoespressif32/cores/esp32/Printable.h
